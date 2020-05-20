8080ecdaf4df88c00ba82e7865f7b99f94eedfdc
Yakovleva Irina <turetskaya30.07.1994@gmail.com>
2020-05-20 20:27:39 +0300

    feat: добавить попап  добавления карточек

--- scripts/script.js
+++ scripts/script.js
@@ -11,25 +11,33 @@ let element = document.querySelector('.my-element');
 */
 
 const profileButtonEdit = document.querySelector('.profile__button-edit');
-const popupButtonClose = document.querySelector('.popup__button-close');
+const popupButtonCloseEdite = document.querySelector('.popup__edite-close');
+const popupButtonCloseAdd = document.querySelector('.popup__add-close');
+const profileButtonAdd = document.querySelector('.profile__button-add');
 const popup = document.querySelector('.popup');
+const popupEdite = document.querySelector('.popup-edite');
+const popupAdd = document.querySelector('.popup-add');
 const formElement = document.querySelector('.popup__form');
 let nameInput = document.querySelector('.popup__input_name');
 let jobInput = document.querySelector('.popup__input_profession');
 let profileName = document.querySelector('.profile__name');
 let profileJob = document.querySelector('.profile__profession');
+let placeInput = document.querySelector('.popup__input_place');
+let linlInput = document.querySelector('.popup__input_link');
 
-
+//открытие - закрытие попапов
 function togglePopup(popup) {
   popup.classList.toggle('popup_opened');
 }
-function showPopup() {
+
+//открытие попапа - редактирование профиля
+function showPopupEdite() {
   nameInput.value = profileName.textContent;
   jobInput.value = profileJob.textContent;
-  togglePopup(popup);
+  togglePopup(popupEdite);
 }
 
-
+//сохранить изменения в профиле
 function formSubmitHandler (evt) {
   evt.preventDefault();
   profileName.textContent = nameInput.value;
@@ -37,6 +45,17 @@ function formSubmitHandler (evt) {
   togglePopup(popup);
 }
 
-profileButtonEdit.addEventListener('click', showPopup);
-popupButtonClose.addEventListener('click', () => togglePopup(popup));
+//открытие попапа - добавления карточки
+function showPopupAdd() {
+  togglePopup(popupAdd);
+}
+
+
+//слушатели для попапа - редактирование профиля
+profileButtonEdit.addEventListener('click', showPopupEdite);
+popupButtonCloseEdite.addEventListener('click', () => togglePopup(popupEdite));
 formElement.addEventListener('submit', formSubmitHandler);
+
+//слушатели для попапа - добавления карточек
+profileButtonAdd.addEventListener('click', showPopupAdd);
+popupButtonCloseAdd.addEventListener('click', () => togglePopup(popupAdd));
